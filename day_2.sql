d5a9b2557a974e20dbcd8ca8ca66895889210c0c
raj.das <rajdas2412@gmail.com>
2026-09-02 08:39:35 -0400

    Day 2

---
+++ day_2.sql
@@ -0,0 +1,18 @@
+select * from emp;
+
+-- Employees and their Managers
+select 
+e.emp_id, e.emp_name, e.salary, e.manager_id, 
+m.emp_name manager_name, m.salary 
+from emp e join emp m
+on e.manager_id = m.emp_id
+order by 1; 
+
+-- Employees whose salary is more than their Managers
+select 
+e.emp_id, e.emp_name, e.salary, e.manager_id, 
+m.emp_name manager_name, m.salary manager_salary
+from emp e join emp m
+on e.manager_id = m.emp_id
+where e.salary > m.salary 
+order by 1;
