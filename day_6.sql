b612d80b940f95319e63fd0ee175676849699443
raj.das <rajdas2412@gmail.com>
2026-09-06 08:42:21 -0400

    Day 6

--- day_6.sql
+++ day_6.sql
@@ -14,7 +14,10 @@ select date '2026-09-04' as todays_date, 3 as n;
  
 -- 0 - Sunday
 -- 1 - Monday
--- ...
+-- 2 - Tuesday
+-- 3 - Wednesday
+-- 4 - Thursday
+-- 5 - Friday
 -- 6 - Saturday
 
 select date_part('dow', now()::date);
